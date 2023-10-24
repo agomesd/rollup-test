f770a00ec5c313d1e82d16960512b583d5f3f140
Alex <alex.gduguay@gmail.com>
2023-10-24 13:16:33 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-slate-900 h-screen flex justify-center items-center flex-col gap-2">
+      <h1 className="text-red-500 text-3xl font-bold">This is a test app</h1>
+      <p className="text-sky-500">With tailwind</p>
+    </div>
+  );
+}
+
+export default App;
